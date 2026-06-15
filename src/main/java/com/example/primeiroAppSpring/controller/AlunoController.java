7073022b00dc3d5301af4a85cd316d0bc1702e14
lucasc14 <lucas.c14.51@gmail.com>
2026-06-15 11:31:14 -0300

    Criando API

---
+++ src/main/java/com/example/primeiroAppSpring/controller/AlunoController.java
@@ -0,0 +1,5 @@
+package com.example.primeiroAppSpring.controller;
+
+public class AlunoController {
+    
+}
---
+++ src/main/java/com/example/primeiroAppSpring/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.primeiroAppSpring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping ("/")
+    public String exibirMensagem() {
+        return "<h1>Hello Word</h1>" +
+                "<p>Meu primeiro app Spring Boot.</p>";
+    }
+
+}
