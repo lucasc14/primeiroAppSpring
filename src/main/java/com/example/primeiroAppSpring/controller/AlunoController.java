169209dad43cc80a09e65ca5b111a746bdab9b16
lucasc14 <lucas.c14.51@gmail.com>
2026-06-15 11:42:19 -0300

    Criando primeira API

--- src/main/java/com/example/primeiroAppSpring/controller/AlunoController.java
+++ src/main/java/com/example/primeiroAppSpring/controller/AlunoController.java
@@ -1,5 +1,17 @@
 package com.example.primeiroAppSpring.controller;
 
+import com.example.primeiroAppSpring.model.Aluno;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AlunoController {
-    
+
+
+    @GetMapping("/api/aluno")
+    public Aluno buscarAluno() {
+        return new Aluno("Lucas", "Desenvolvimento de sistemas");
+    }
+
 }
