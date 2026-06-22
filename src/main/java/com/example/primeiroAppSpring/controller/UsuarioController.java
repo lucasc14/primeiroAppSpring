25e6af50f936c0072279a024b8251732cd9b524a
lucasc14 <lucas.c14.51@gmail.com>
2026-06-22 09:23:57 -0300

    Criando o POST de cadastro

--- src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
+++ src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.example.primeiroAppSpring.controller;
 
 import com.example.primeiroAppSpring.model.UsuarioForm;
+import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.Normalizer;
 
 @Controller
 @RequestMapping("/")
@@ -16,6 +21,17 @@ public class UsuarioController {
         model.addAttribute("subTituloPagina", "Sistema de Gerenciamneto de Estoque da Cozinha");
 
         return "cadastro";
+    }
+
+    @PostMapping("/cadastro")
+    public String processarUsuario(@ModelAttribute UsuarioForm form, Model model) {
+        if (!form.getSenha().equals(form.getConfirmaSenha())){
+            model.addAttribute("erro", "Erro, as senhas não conferem !");
+            return "cadastro";
+        }
+        IO.println("Usuário criado com sucesso");
+        IO.println(form.getNome()+ " "+form.getSenha());
+        return "redirect:/login";
     }
     @GetMapping("/login")
     public String exibirLogin(Model model) {
