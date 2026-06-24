0272cd7420c7c4bf1d414be3c38ee2168fa057d3
lucasc14 <lucas.c14.51@gmail.com>
2026-06-24 11:27:57 -0300

    Configurando as estrututas de pastas. E criando as as classes repository e service.

--- src/main/java/com/example/primeiroAppSpring/model/Usuario.java
+++ src/main/java/com/example/primeiroAppSpring/model/Usuario.java
@@ -1,4 +1,51 @@
 package com.example.primeiroAppSpring.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    protected Usuario() {
+    }
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
--- src/main/java/com/example/primeiroAppSpring/repository/UsuarioRepository.java
+++ src/main/java/com/example/primeiroAppSpring/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.example.primeiroAppSpring.repository;
 
-public class UsuarioRepository {
+import com.example.primeiroAppSpring.model.Usuario;
+import org.hibernate.internal.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByEmail ( String email);
+
+    boolean existsByEmail ( String email);
+    
 }
